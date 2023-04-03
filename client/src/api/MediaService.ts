0b4f9efed4a88dbf981913dc89efa3d4ba0e44ba
HaiDang309 <hada.nguyen309@gmail.com>
2023-04-03 11:21:41 +0700

    fix: change api route

--- client/src/api/MediaService.ts
+++ client/src/api/MediaService.ts
@@ -1,42 +1,49 @@
 import { AxiosProgressEvent } from "axios";
 
+import { IAxiosResponse } from "src/interfaces";
 import request from "src/utilities/request";
 
 class MediaService {
-	static getListMedia(id) {
+	static getListMedia(id): Promise<IAxiosResponse> {
 		return request({
 			method: "GET",
-			url: `/v1/auth/medias?exhibitionId=${id}`,
+			url: `/v1/auth/media?exhibitionId=${id}`,
 		});
 	}
 
 	static proxyMedia(objectId) {
 		return request({
 			method: "GET",
-			url: `/v1/medias/proxy/${objectId}`,
+			url: `/v1/media/proxy/${objectId}`,
 		});
 	}
 
 	static updateMediaMany(data) {
 		return request({
 			method: "PUT",
-			url: `/v1/auth/medias`,
+			url: `/v1/auth/media`,
 			data: {
 				medias: data,
 			},
 		});
 	}
 
-	static upload(file: File, onProgress: (percent: number) => void, abortController: AbortController | null) {
+	static upload(
+		file: File,
+		onProgress: (percent: number) => void,
+		abortController: AbortController | null,
+	) {
 		const data = new FormData();
 		data.append("file", file);
-		return request.post("/v1/auth/medias/upload", data, {
+		return request.post("/v1/auth/media/upload", data, {
 			headers: {
 				"Content-Type": "multipart/form-data",
 			},
 			onUploadProgress: (progressEvent: AxiosProgressEvent) => {
 				if (progressEvent?.loaded && progressEvent?.total) {
-					const percent = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+					const percent = Math.round(
+						(progressEvent.loaded * 100) / progressEvent.total,
+					);
 					if (onProgress) onProgress(percent);
 				}
 			},
