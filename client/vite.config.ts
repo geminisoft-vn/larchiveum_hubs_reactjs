d33bf3df73a9f173a782ac7e59e25dbb5d7a38f9
HaiDang309 <hada.nguyen309@gmail.com>
2023-04-10 14:52:32 +0700

    fix: change ouput dir's name

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,10 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	root: "src",
+	build: {
+		outDir: "../www",
+	},
 	resolve: {
 		alias: [
 			{
