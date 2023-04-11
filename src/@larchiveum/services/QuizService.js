6e29ba952012c47f37568e9a341b9a0690e39812
Orics <nguyenngockhai25@gmail.com>
2023-04-11 17:57:52 +0700

    fix content: document,quiz

---
+++ src/@larchiveum/services/QuizService.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import request from "./request";
+
+class QuizService {
+	static getAll(params) {
+		return request.get("/v1/auth/quizzes", { params });
+	}
+
+	static getOne(id) {
+		return request.get(`/v1/auth/quizzes/${id}`);
+	}
+
+	static create(data) {
+		return request.post("/v1/auth/quizzes", data);
+	}
+
+	static update(id, data) {
+		return request.put(`/v1/auth/quizzes/${id}`, data);
+	}
+
+	static delete(id) {
+		return request.delete(`/v1/auth/quizzes/${id}`);
+	}
+}
+
+export default QuizService;
--- src/react-components/room/DocumentContentModal.js
+++ src/react-components/room/DocumentContentModal.js
@@ -3,20 +3,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { Modal } from "../modal/Modal";
-import { Button } from "../input/Button";
 import { CloseButton } from "../input/CloseButton";
-import { ReactComponent as EnterIcon } from "../icons/Enter.svg";
-import { ReactComponent as VRIcon } from "../icons/VR.svg";
-import { ReactComponent as ShowIcon } from "../icons/Show.svg";
-import { ReactComponent as SettingsIcon } from "../icons/Settings.svg";
-import { ReactComponent as HmcLogo } from "../icons/HmcLogo.svg";
 import styles from "./DocumentContentModal.scss";
-import styleUtils from "../styles/style-utils.scss";
-import { useCssBreakpoints } from "react-use-css-breakpoints";
 import { Column } from "../layout/Column";
 import { FormattedMessage } from "react-intl";
-import configs from "../../utils/configs";
-import Store from "../../@larchiveum/utilities/store";
 
 export function DocumentContentModal({ className, documentUrl, closeable, onClose }) {
   return (
