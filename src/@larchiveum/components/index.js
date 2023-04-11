da0402b7d180bdbdda630d1a666e84a3362b719c
HaiDang309 <hada.nguyen309@gmail.com>
2023-04-11 16:02:12 +0700

    refactor: revert old home page

--- src/@larchiveum/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Header } from "./Header";
-export { default as SigninSocialButton } from "./SigninSocialButton";
-export { default as ToggleInput } from "./ToggleInput";
--- src/index.js
+++ src/index.js
@@ -1,71 +1,29 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom";
-
-import "./react-components/styles/global.scss";
-
+import { WrappedIntlProvider } from "./react-components/wrapped-intl-provider";
+import registerTelemetry from "./telemetry";
 import Store from "./storage/store";
 import "./utils/theme";
+import { HomePage } from "./react-components/home/HomePage";
+import { AuthContextProvider } from "./react-components/auth/AuthContext";
+import "./react-components/styles/global.scss";
+import { ThemeProvider } from "./react-components/styles/theme";
 
-import {
-  HomePage,
-  SigninPage,
-  SignupPage,
-  WarningVerifyPage,
-  ContentPage,
-  ManagerPage,
-  ForgotPasswordPage,
-  ResetPasswordPage,
-  ProfilePage,
-  CallbackNaverOAuthPage,
-  ViewQuizPage
-} from "src/@larchiveum/pages";
+registerTelemetry("/home", "Hubs Home Page");
 
 const store = new Store();
 window.APP = { store };
 
-const getPage = () => {
-  return new URL(location.href).searchParams.get("page");
-};
-
 function Root() {
-  const page = getPage();
-  switch (page) {
-    case "home":
-      return <HomePage />;
-
-    case "signin":
-      return <SigninPage />;
-
-    case "signup":
-      return <SignupPage />;
-    case "warning-verify":
-      return <WarningVerifyPage />;
-
-    case "content":
-      return <ContentPage />;
-
-    case "manager":
-      return <ManagerPage />;
-
-    case "forgot-password":
-      return <ForgotPasswordPage />;
-
-    case "reset-password":
-      return <ResetPasswordPage />;
-
-    case "profile":
-      return <ProfilePage props={{ avarar: "Avt", name: "Name" }} />;
-
-    case "callback-naver-oauth":
-      return <CallbackNaverOAuthPage />;
-
-    case "view-quiz":
-      return <ViewQuizPage />;
-
-    default:
-      return <HomePage />;
-  }
+  return (
+    <WrappedIntlProvider>
+      <ThemeProvider store={store}>
+        <AuthContextProvider store={store}>
+          <HomePage />
+        </AuthContextProvider>
+      </ThemeProvider>
+    </WrappedIntlProvider>
+  );
 }
 
 ReactDOM.render(<Root />, document.getElementById("home-root"));
