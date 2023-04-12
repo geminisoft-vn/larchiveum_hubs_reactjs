6bee28e8a4682561d8cdb422fc5c73ed4abacf78
HaiDang309 <hada.nguyen309@gmail.com>
2023-04-12 14:28:57 +0700

    make hub work

--- src/index.js
+++ src/index.js
@@ -28,43 +28,7 @@ const getPage = () => {
 };
 
 function Root() {
-  const page = getPage();
-  switch (page) {
-    case "home":
-      return <HomePage />;
-
-    case "signin":
-      return <SigninPage />;
-
-    case "signup":
-      return <SignupPage />;
-    case "warning-verify":
-      return <WarningVerifyPage />;
-
-    case "content":
-      return <ContentPage />;
-
-    case "manager":
-      return <ManagerPage />;
-
-    case "forgot-password":
-      return <ForgotPasswordPage />;
-
-    case "reset-password":
-      return <ResetPasswordPage />;
-
-    case "profile":
-      return <ProfilePage props={{ avarar: "Avt", name: "Name" }} />;
-
-    case "callback-naver-oauth":
-      return <CallbackNaverOAuthPage />;
-
-    case "view-quiz":
-      return <ViewQuizPage />;
-
-    default:
-      return <HomePage />;
-  }
+  return <div>HomePage</div>;
 }
 
 ReactDOM.render(<Root />, document.getElementById("home-root"));
