4d972c899ace465ed8a88cc847118fe945491040
HaiDang309 <hada.nguyen309@gmail.com>
2023-04-13 10:06:55 +0700

    revert old index file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 import { WrappedIntlProvider } from "./react-components/wrapped-intl-provider";
 import registerTelemetry from "./telemetry";
 import Store from "./storage/store";
-import { useEffect } from "react";
+import "./utils/theme";
+import { HomePage } from "./react-components/home/HomePage";
+import { AuthContextProvider } from "./react-components/auth/AuthContext";
+import "./react-components/styles/global.scss";
+import { ThemeProvider } from "./react-components/styles/theme";
 
 registerTelemetry("/home", "Hubs Home Page");
 
@@ -11,23 +15,14 @@ const store = new Store();
 window.APP = { store };
 
 function Root() {
-  useEffect(() => {
-    window.addEventListener("message", function (event) {
-        if (event.origin !== 'https://hubs.local:8080') {
-          document.querySelector('h2#dang').innerHTML = event.data
-        
-        return;
-      }
-      console.log("received: " + event.data);
-    });
-  }, [])
   return (
-    <div>
-      <center>
-        <h1>{"Wellcome"}</h1>
-        <h2 id="dang"></h2>
-      </center>
-    </div>
+    <WrappedIntlProvider>
+      <ThemeProvider store={store}>
+        <AuthContextProvider store={store}>
+          <HomePage />
+        </AuthContextProvider>
+      </ThemeProvider>
+    </WrappedIntlProvider>
   );
 }
 
