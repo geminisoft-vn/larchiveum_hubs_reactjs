3646e53d9dfccee1cbb50d6f512f525b8d325472
HaiDang309 <hada.nguyen309@gmail.com>
2023-05-05 12:45:59 +0700

    fix: get all exhibitions without auth

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -27,7 +27,7 @@ const HomePage = () => {
 	});
 
 	const getAllExhibitions = useCallback(() => {
-		ExhibitionsService.getAllWithAuthExhibitions(params)
+		ExhibitionsService.getAllExhibitions(params)
 			.then((res) => {
 				if (res.result === "ok") {
 					dispatch(
